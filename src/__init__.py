4bfc79e2d9a3c5f10c8778ec188340f3d877381a
jiali li <181945142+ljlbit21@users.noreply.github.com>
2026-07-02 22:44:06 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
-# ChromoNet Package
+from .model import ChromoNet
+from .preprocessor import SmartPreprocessor
+from .trainer import train_model
+from .mae import MaskedAutoencoder1D, pretrain_mae
+from . import visualizer
+from . import convert_data
+
+__version__ = '1.8.0'
